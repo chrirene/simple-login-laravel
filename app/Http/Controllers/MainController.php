a14cd936185079c4ccd2f822ca02234958c2a551
chrirene <chrirene7@gmail.com>
2022-04-23 22:04:16 +0700

    initial commit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,53 +3,112 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use App\Models\Role;
-//use Auth;
 
 class MainController extends Controller
 {
     function index()
     {
-     return view('login');
+        return view('login');
     }
 
+    // login using Laravel Auth framework
     function checklogin(Request $request)
     {
-     $this->validate($request, [
-      'email'   => 'required|email',
-      'password'  => 'required|alphaNum|min:3'
-     ]);
+        // validate email & password
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password'  => 'required|alphaNum|min:3'
+        ]);
 
-     $user_data = array(
-      'email'  => $request->get('email'),
-      'password' => $request->get('password')
-     );
+        $user_data = array(
+            "email"  => $request->get('email'),
+            "password" => $request->get('password')
+        );
 
-     if(Auth::attempt($user_data))
-     {
-      return redirect('main/successlogin');
-     }
-     else
-     {
-      return back()->with('error', 'Wrong Login Details');
-     }
-
+        // attempt login using Auth
+        $data = DB::table('users')->first();
+        $attempt = Auth::attempt($user_data);
+        if (Auth::attempt($user_data)) {
+            // get user role data
+            $role =  DB::table('roles')->where('role_id', Auth::user()->role_id)->value('name');
+            if ($role != null) {
+                return redirect('main/successlogin');
+            } else {
+                return back()->with('error', 'Wrong Login Details');
+            }
+        } else {
+            return back()->with('error', 'Wrong Login Details');
+        }
     }
 
+    // route view to dashboard based on user role
     function successlogin()
     {
         $role = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('name');
         $desc = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('description');
-        return view('successlogin')->with('role', $role)->with('description', $desc);
-     //return view('successlogin');
+
+        switch ($role) {
+            case 'Manager':
+                return view('dashboardM')->with('role', $role)->with('description', $desc);
+                break;
+            case 'Staff':
+                return view('staffDashboard')->with('role', $role)->with('description', $desc);
+                break;
+            default:
+                return back()->with('error', 'Undefined Role');
+                break;
+        }
     }
 
+    // route view to user list page (only accessible by manager)
+    function userview()
+    {
+        $role = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('name');
+        $desc = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('description');
+        return view('userM')->with('role', $role)->with('description', $desc);
+    }
+
+    // route view to user profile page
+    function profileview()
+    {
+        $role = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('name');
+        $desc = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('description');
+
+        switch ($role) {
+            case 'Manager':
+                return view('profileM')->with('role', $role)->with('description', $desc);
+                break;
+            case 'Staff':
+                return view('staffProfile')->with('role', $role)->with('description', $desc);
+                break;
+            default:
+                return back()->with('error', 'Undefined Role');
+                break;
+        }
+    }
+
+    // route view to employee performance page (only accessible by manager)
+    function employeeview()
+    {
+        $role = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('name');
+        $desc = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('description');
+        return view('employeeM')->with('role', $role)->with('description', $desc);
+    }
+
+    // route view to project list page (only accessible by employee)
+    function projectListView()
+    {
+        $role = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('name');
+        $desc = DB::table('roles')->where('role_id', Auth::user()->role_id)->value('description');
+        return view('staffProjectList')->with('role', $role)->with('description', $desc);
+    }
+
+    // Auth logout
     function logout()
     {
-     Auth::logout();
-     return redirect('main');
+        Auth::logout();
+        return redirect('main');
     }
 }
